6df7ebe71a1c43eecd9bc441d4566284daceeb4c
sembeknurmukhamed <258036382+sembeknurmukhamed@users.noreply.github.com>
2026-02-17 13:13:31 +0500

    Add math

---
+++ practice-4/math.py
@@ -0,0 +1,21 @@
+# The min() and max() functions can be used to find the lowest or highest value in an iterable
+# The abs() function returns the absolute (positive) value of the specified number
+# The pow(x, y) function returns the value of x to the power of y (x^y)
+
+# Python has also a built-in module called math, which extends the list of mathematical functions
+import math
+
+x = math.sqrt(64)
+print(x)
+
+import math
+
+x = math.ceil(1.1)
+y = math.floor(1.6)
+
+print(x) # returns 2
+print(y) # returns 1 
+
+# The math.pi constant, returns the value of PI (3.14...)
+x = math.pi
+print(x)
